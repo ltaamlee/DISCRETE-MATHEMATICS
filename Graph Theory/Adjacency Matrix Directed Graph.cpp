ef073d909d5ef48d504b03dafcf20df004d42dca
LTaam <167110901+ltaamlee@users.noreply.github.com>
2024-04-28 19:37:29 +0700

    Adjacency Matrix Directed Graph.cpp

--- Graph Theory/Adjacency Matrix Directed Graph.cpp
+++ Graph Theory/Adjacency Matrix Directed Graph.cpp
@@ -9,8 +9,8 @@ void INPUT(int &n, int a[][nmax]){
 		}
 	}
 }
-void PARALLEL(set<tuple<int,int>> &st){
-	for (auto x: st){
+void PARALLEL(vector<tuple<int,int>> &v){
+	for (auto x: v){
 		tuple<int,int> t=x;
 		cout<<"("<<get<0>(x)<<","<<get<1>(x)<<")\n";
 	}
@@ -18,22 +18,22 @@ void PARALLEL(set<tuple<int,int>> &st){
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	int n, a[nmax][nmax], cnt=0;
-	set <tuple<int,int>> st1, st2;
+	vector <tuple<int,int>> v1, v2;
 	INPUT(n,a);
 	cout<<"-------------------\n";
-	cout<<"  DO THI VO HUONG \n";
+	cout<<" MA TRAN KE DO THI VO HUONG \n";
 	cout<<"-------------------\n";
 //a. Cac cap dinh co canh song song cung huong
 	cout<<"Cap dinh co canh song song cung huong:\n";
 	for (int i=1; i<=n; ++i){
 		for (int j=1; j<=n; ++j){
 			if (a[i][j]==2){
-				st1.insert(make_tuple(i,j));
+				v1.push_back(make_tuple(i,j));
 			}
 		}
 	}
-	if (st1.size()>0)
-		PARALLEL(st1);
+	if (v1.size()>0)
+		PARALLEL(v1);
 	else cout<<"Khong co!\n";
 	cout<<"------------------\n";
 //b. Cac cap dinh co canh song song nguoc huong
@@ -42,13 +42,13 @@ int main(){
 	for (int i=1; i<=n; ++i){
 		for (int j=1; j<=n; ++j){
 			if (a[i][j]==1 && a[j][i]==1 && check[i][j]==false && check[i][j]==false){
-				st2.insert(make_tuple(i,j));
+				v2.push_back(make_tuple(i,j));
 				check[i][j]=check[j][i]=true;
 			}
 		}
 	}
-	if (st2.size()>0)
-		PARALLEL(st2);
+	if (v2.size()>0)
+		PARALLEL(v2);
 	else cout<<"Khong co!\n";
 	cout<<"------------------\n";
 //c. Ban bac trong, ban bac ngoai 
